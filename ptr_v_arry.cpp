17081cf2a9abed8234a417f976264ef29700d6be
billy wong <ppucoder@gmail.com>
2014-08-28 23:16:25 +0300

    Update ptr_v_arry.cpp

--- ptr_v_arry.cpp
+++ ptr_v_arry.cpp
@@ -7,9 +7,60 @@
 #define SIZE_D3 7
 
 using namespace std; 
-void foo(char array[][SIZE_D1]);
-void foo2d(char** d2_arry_ptr, int d2, int d1);
-void foo3d(char** d3_arry_ptr, int d3, int d2, int d1);
+void foo_arry2d(char _array[][SIZE_D1])
+{
+	for(int i=0; i<SIZE_D2 ;i++)
+	{
+		for(int j=0; j<SIZE_D1 ;j++) //now go through inside d1_ptr[i]
+		{
+			printf("%c ",_array[i][j]);//row first visit is in the most inner loop.
+		}
+		printf("\n ");
+	}
+}
+void foo_arry3d(char _array[][SIZE_D2][SIZE_D1])
+{
+	for(int k=0; k<SIZE_D3 ;k++)
+	{
+		for(int i=0; i<SIZE_D2 ;i++) //now go through inside d1_ptr[k] total SIZE_D2*SIZE_D1 element
+		{
+			for(int j=0; j<SIZE_D1 ;j++) //now go through inside d1_ptr[k][i]
+			{
+				printf("%c ",_array[k][i][j]);//row first visit is in the most inner loop.
+			}
+			printf("\n ");
+		}
+	}
+}
+void foo2d(char** d2_arry_ptr, int d2, int d1)
+{
+	
+	for(int i=0; i<d2 ;i++)
+	{
+		for(int j=0; j<d1 ;j++)
+		{
+			printf("%c ",(*d2_arry_ptr)[j+i*d1] );
+		}
+		printf("\n ");
+	}
+}
+
+void foo3d(char** d3_arry_ptr, int d3, int d2, int d1)
+{
+	for(int k=0; k<SIZE_D3 ;k++)
+	{
+		for(int i=0; i<SIZE_D2 ;i++)
+		{
+			for(int j=0; j<SIZE_D1 ;j++) 
+			{
+				printf("%c ",(*d3_arry_ptr)[i*d1+j+k*d1*d2] );
+			}
+			printf("\n ");
+		}
+		printf("\n ===================================== \n");
+	}
+}
+
 void main()  
 {  
     const char *pInputStr="xxxyyyeeeellllllllddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddkkkkkkkkkkkkzzzzzzzzzzzzdddddddddgggggggzza";
@@ -40,47 +91,8 @@ void main()
 
 	foo2d((char**)&d1_ptr, SIZE_D2, SIZE_D1);
 	foo3d((char**)&d1_ptr, SIZE_D3,SIZE_D2, SIZE_D1);
-	foo(d1_ptr);
+	foo_arry2d(d1_ptr);
+	foo_arry3d(a_d1_ptr);
 	printf("\n ");
 }
 
-void foo(char _array[][SIZE_D1])
-{
-	for(int i=0; i<SIZE_D2 ;i++)
-	{
-		for(int j=0; j<SIZE_D1 ;j++) //now go through inside d1_ptr[i]
-		{
-			printf("%c ",_array[i][j]);//row first visit is in the most inner loop.
-		}
-		printf("\n ");
-	}
-}
-
-void foo2d(char** d2_arry_ptr, int d2, int d1)
-{
-	
-	for(int i=0; i<d2 ;i++)
-	{
-		for(int j=0; j<d1 ;j++) //now go through inside d1_ptr[i]
-		{
-			printf("%c ",(*d2_arry_ptr)[j+i*d1] );//row first visit is in the most inner loop.
-		}
-		printf("\n ");
-	}
-}
-
-void foo3d(char** d3_arry_ptr, int d3, int d2, int d1)
-{
-	for(int k=0; k<SIZE_D3 ;k++)
-	{
-		for(int i=0; i<SIZE_D2 ;i++)
-		{
-			for(int j=0; j<SIZE_D1 ;j++) //now go through inside d1_ptr[i]
-			{
-				printf("%c ",(*d3_arry_ptr)[i*d1+j+k*d1*d2] );//row first visit is in the most inner loop.
-			}
-			printf("\n ");
-		}
-		printf("\n ===================================== \n");
-	}
-}
